e0179244341d6a26f57b8383a46019038988fe3e
Liam Ma <liam.q.ma@gmail.com>
2013-12-15 22:55:09 +1100

    Update ServiceProvider

--- src/Liamqma/AuthToken/AuthTokenRepository.php
+++ src/Liamqma/AuthToken/AuthTokenRepository.php
@@ -5,17 +5,17 @@ namespace Liamqma\AuthToken;
 class AuthTokenRepository {
 
     public function purge($userId, $userTable) {
-        DB::table('AuthToken')->where('user_id', '=', $userId)->where('user_table', '=', $userTable)->delete();
+        \DB::table('AuthToken')->where('user_id', '=', $userId)->where('user_table', '=', $userTable)->delete();
     }
 
     public function store($userId, $userTable, $publicKey, $privateKey) {
-        DB::table('AuthToken')->insert(
+        \DB::table('AuthToken')->insert(
                 array('user_id' => $userId, 'user_table' => $userTable, 'public_key'=>$publicKey, 'private_key'=>$privateKey)
         );
     }
 
     public function getOrFail($userId, $publicKey, $userTable) {
-                $result = DB::table('AuthToken')->insert(
+                $result = \DB::table('AuthToken')->insert(
                 array('user_id' => $userId, 'public_key'=>$publicKey, 'user_table' => $userTable) 
         )->first();
                 if($result)
--- src/Liamqma/AuthToken/AuthTokenServiceProvider.php
+++ src/Liamqma/AuthToken/AuthTokenServiceProvider.php
@@ -1,34 +1,46 @@
-<?php namespace Liamqma\AuthToken;
+<?php
+
+namespace Liamqma\AuthToken;
 
 use Illuminate\Support\ServiceProvider;
 
 class AuthTokenServiceProvider extends ServiceProvider {
 
-	/**
-	 * Indicates if loading of the provider is deferred.
-	 *
-	 * @var bool
-	 */
-	protected $defer = false;
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = false;
 
-	/**
-	 * Register the service provider.
-	 *
-	 * @return void
-	 */
-	public function register()
-	{
-		//
-	}
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register() {
+        $app = $this->app;
 
-	/**
-	 * Get the services provided by the provider.
-	 *
-	 * @return array
-	 */
-	public function provides()
-	{
-		return array();
-	}
+        $app->bind('liamqma.auth.token.repo', function () {
+                    return new AuthTokenRepository();
+                });
+        $app->bind('liamqma.auth.token.hash', function () {
+                    return new HashProvider();
+                });
+        $app->bind('liamqma.auth.token', function () {
+                    return new AuthToken($app->make('liamqma.auth.token.repo'),$app->make('liamqma.auth.token.hash'), new \Encrypter);
+                });
+                
+                
+    }
+
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides() {
+        return array();
+    }
 
 }
